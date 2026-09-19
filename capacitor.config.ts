e9b94504a80cd35bc0c75eae434c6d28bfb71902
Raazim tech <raazimtech@gmail.com>
2026-09-19 12:53:58 +0300

    Replace Ciwaan with Asal Pay demo MVP (#1)

--- capacitor.config.ts
+++
@@ -1,3 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli'
-const config: CapacitorConfig = { appId:'com.risingtech.ciwaan', appName:'Ciwaan', webDir:'dist', server:{ androidScheme:'https' } }
-export default config
